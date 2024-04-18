3b997506d35f4a8a783461b420ba8c4416570f88
Brian H. Buchholz (GitHub) <4773480+bhb603@users.noreply.github.com>
2024-04-18 17:00:23 -0400

    update deno install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-deno install -f ./json2tsv.ts
-deno install -f --allow-env --allow-read --allow-sys --allow-net ./lambdaDotenv.ts
-deno install -f --allow-write ./scriptMe.ts
+deno install -gf ./json2tsv.ts
+deno install -gf --allow-env --allow-read --allow-sys --allow-net ./lambdaDotenv.ts
+deno install -gf --allow-write ./scriptMe.ts
