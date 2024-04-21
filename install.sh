16d13a65267d9e52aeca1142ba1a4aa9dba47eff
Brian H. Buchholz <4773480+bhb603@users.noreply.github.com>
2024-04-21 15:27:37 -0400

    update json2tsv

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-deno install -gf ./json2tsv.ts
+deno install -gf --allow-read ./json2tsv.ts
 deno install -gf --allow-env --allow-read --allow-sys --allow-net ./lambdaDotenv.ts
 deno install -gf --allow-write ./scriptMe.ts
--- json2tsv.ts
+++ json2tsv.ts
@@ -1,15 +1,23 @@
-#!/usr/bin/env -S deno run
+#!/usr/bin/env -S deno run --allow-read
 
 import { parseArgs, TextLineStream } from "./deps.ts";
 
 const USAGE = `Convert JSON to TSV format.
+Input can be either a file or stdin
+
+Usage:
+  json2tsv [--input <file>]
+
 Example:
   cat file.json | json2tsv
+
+  json2tsv --input data.json
 `;
 
 const args = parseArgs(Deno.args, {
   alias: {
     h: "help",
+    i: "input",
   },
 });
 
@@ -18,22 +26,21 @@ if (args.help) {
   Deno.exit(0);
 }
 
-const jsonToTSVStream = new WritableStream({
-  write(chunk) {
-    try {
-      const obj = JSON.parse(chunk);
-      const tsv: string[] = [];
-      Object.values(obj).forEach((value) => {
-        tsv.push(JSON.stringify(value));
-      });
-      console.log(tsv.join("\t"));
-    } catch (error) {
-      console.error(error.message);
-    }
-  },
-});
+const input = args.input ? await Deno.open(args.input) : Deno.stdin;
 
-Deno.stdin.readable
+const lineStream = input.readable
   .pipeThrough(new TextDecoderStream())
-  .pipeThrough(new TextLineStream())
-  .pipeTo(jsonToTSVStream);
+  .pipeThrough(new TextLineStream());
+
+for await (const line of lineStream) {
+  try {
+    const obj = JSON.parse(line);
+    const tsv: string[] = [];
+    Object.values(obj).forEach((value) => {
+      tsv.push(JSON.stringify(value));
+    });
+    console.log(tsv.join("\t"));
+  } catch (error) {
+    console.error(error.message);
+  }
+}
